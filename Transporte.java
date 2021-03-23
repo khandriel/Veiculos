943cf619e9157c08770302364ef47b2185b572c8
Osmar <Osmar.S@edu,pucrs.br>
2021-03-23 15:44:41 -0300

    Criação da classe transporte

---
+++ Transporte.java
@@ -0,0 +1,22 @@
+public class Transporte extends Veiculo{
+
+    private int passageiros;
+    private String revestimento;
+
+    public Transporte(int preco, int ano, float motor, String cor, String tracao,
+        int passageiros, String revestimento){
+        
+        super(preco, ano, motor, cor, tracao);
+        this.passageiros = passageiros;
+        this.revestimento = revestimento;
+    }
+
+    public int getPassageiros(){
+        return passageiros;
+    }
+
+    public String getRevestimento(){
+        return revestimento;
+    }
+
+}
