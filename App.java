b8f19539ba095eec231671ad86c3e5e885ea4911
Arthur-Gil-S <A.Gil@edu.pucrs.br>
2021-03-24 18:17:02 -0300

    Alteracoes finais classe App

--- App.java
+++ App.java
@@ -2,14 +2,14 @@ public class App {
     public static void main (String[] args){
         Catalogo c = new Catalogo();
 
-        Veiculo ftype = new Esportivo(310000, 2019, 2.0, "branco", "AWD", 300, 2);
-        Veiculo sian = new Esportivo(13323000, 2020, 2.0, "preto", "intg", 350, 2);
-        Veiculo elise = new Esportivo(520000, 2019, 1.8, "cinza", "traseira", 200, 2);
+        Veiculo ftype = new Esportivo("F-Type", 310000, 2019, 2.0, "branco", "AWD", 300, 2);
+        Veiculo sian = new Esportivo("Lambo Sian", 13323000, 2020, 2.0, "preto", "intg", 350, 2);
+        Veiculo elise = new Esportivo("Lotus Elise", 520000, 2019, 1.8, "cinza", "traseira", 200, 2);
 
-        Veiculo troller = new Trilha(150000, 2020, 3.0, "verde", "4x4", true, 1);
-        Veiculo wrangler = new Trilha(200000, 2019, 2.0, "cinza", "4x4", false, 1);
+        Veiculo troller = new Trilha("Troller", 150000, 2020, 3.0, "verde", "4x4", true, 1);
+        Veiculo wrangler = new Trilha("Wrangler", 200000, 2019, 2.0, "cinza", "4x4", false, 1);
 
-        Veiculo boxer = new Transporte(100000, 2015, 2.0, "branco", "traseira", 10, 0);
+        Veiculo boxer = new Transporte("Boxer", 100000, 2015, 2.0, "branco", "traseira", 10, 0);
 
         c.addCatalogo(ftype);
         c.addCatalogo(sian);
