e618f42573e44741600123d8f17aba1bddd3d9b0
Arthur-Gil-S <A.Gil@edu.pucrs.br>
2021-03-24 17:19:11 -0300

    Alteracoes UI

--- UI.java
+++ UI.java
@@ -4,36 +4,33 @@ public class UI{
     public void Ui(){
         Scanner in = new Scanner(System.in);
 
-        //Catalogo cat = new Catalogo();
+        Catalogo cat = new Catalogo();
 
         boolean execut = true;
         while(execut){
             System.out.println("---Interface---");
-            System.out.println("1 - Carros a venda");
-            System.out.println("2 - Carros esportivos");
-            System.out.println("3 - Carros de trilha");
+            System.out.println("1 - Veiculos a venda");
+            System.out.println("2 - Busca por ano");
+            System.out.println("3 - Busca por motor");
             System.out.println("4 - Sair");
 
             String s = in.next();
 
             switch(s){
                 case "1":
-                    //Lista todos os carros a venda do catalogo
+                    System.out.println("---> Catalogo");
+                    System.out.println(cat.imprimeCat());
 
-
-                case "2":
-                    //Lista carros esportivos a venda
-
+                case "2":                 
+                    System.out.println(cat.getPorAno());
 
                 case "3":
-                    //Lista carros de trilha a venda 
-                
-
+                    System.out.println(cat.getPorMotor());
+            
                 case "4":
-                    System.out.println("Venda cancelada.");
+                    System.out.println("Saindo...");
                     break;
-                    
-
+                
                 default: break;
                 
             }
